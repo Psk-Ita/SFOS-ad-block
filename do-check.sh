7518f6488e00a54a766dce199af1fbcb23b264dd
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2025-08-25 13:51:42 +0200

    Update do-check.sh

--- do-check.sh
+++ do-check.sh
@@ -2,7 +2,7 @@
 MODDIR=${0%/*}
 
 # if temp folder exists - delete
-TMP="/data/local/tmp/ad-block"
+TMP="/tmp/ad-block"
 if [[ -d "${TMP}" ]]; then
   rm -rf "${TMP}"
 fi
@@ -10,13 +10,14 @@ fi
 sleep 5
 
 # check last update
-OUT=${MODDIR}/system/etc/hosts
+OUT=${MODDIR}/last
 NOW="## $(date '+%Y-%m-%d') ##"
 LST=""
 if [[ -f "${OUT}" ]]; then
   LST=$(head -n 1 ${OUT})
 fi
 if [[ "${LST}" == "${NOW}" ]]; then
+  echo "already up to date"
   return 0
 fi
 
